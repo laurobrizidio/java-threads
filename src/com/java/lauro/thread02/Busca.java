2e1b739bf1bc69e84d6dc38b250c9a269e66fa10
Lauro Brizidio <lauro.brizidio@itbam.org.br>
2021-03-15 14:27:14 -0400

    feat: search text in .txt archives

--- src/com/java/lauro/thread02/Busca.java
+++ src/com/java/lauro/thread02/Busca.java
@@ -1,7 +1,19 @@
 package com.java.lauro.thread02;
 
+import java.util.Scanner;
+
 public class Busca {
     public static void main(String[] args) {
+        String nome  = new Scanner(System.in).next();
 
+
+
+        Thread thread01 = new Thread(new TaskBuscaTexto("assinaturas1.txt", nome));
+        Thread thread02 = new Thread(new TaskBuscaTexto("assinaturas2.txt", nome));
+        Thread thread03 = new Thread(new TaskBuscaTexto("autores.txt", nome));
+
+        thread01.start();
+        thread02.start();
+        thread03.start();
     }
 }
